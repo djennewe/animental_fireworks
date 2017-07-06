879a6615737cbdbd38a6fc19bc1e2c5f05c44e80
djennewe <doug@Dougs-MacBook-Air.local>
2017-07-06 18:29:55 -0500

    sort of centered

--- af.py
+++ af.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 
+
 import random
+import textwrap
 from PIL import Image, ImageFont, ImageDraw
 
-fontSize = 28
+fontSize = 44
+
+lineWidth = 15 # characters
 
 #font = ImageFont.load_default().font
 font = ImageFont.truetype("Flame.ttf", fontSize)
 
-#im=Image.open("package.jpg")
 im=Image.open("explosion.jpg")
 draw = ImageDraw.Draw(im)
 
@@ -23,9 +26,21 @@ with open(elements_filename) as elements_file:
 	elements = elements_file.readlines()
 elements = [x.strip() for x in elements]
 
-#message = "Celebrate the 4th with..." + random.choice(elements) + " " + random.choice(animals)
 message = random.choice(elements) + " " + random.choice(animals)
 
-# TODO: Center text on box
-draw.text((10, 10), message, font=font)
+#txtw, txth = font.getsize(message)
+#x = (im.width - txtw) / 2
+#y = (im.height - txth) / 2
+#draw.text((x, y), message, font=font)
+
+para = textwrap.wrap(message, lineWidth)
+w, h = font.getsize("foo")
+msgHeight = h * len(para)
+cur_h, pad = (im.height - msgHeight) / 2 , 10
+
+for line in para:
+	w, h = font.getsize(line)
+	draw.text(((im.width - w) / 2, cur_h), line, font=font)
+	cur_h += h + pad
+
 im.show()
